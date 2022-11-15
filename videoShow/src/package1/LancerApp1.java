878a06ab5b06dc29003eb149be113dba5791e077
unknown <meftahahananiama@gmail.com>
2022-11-15 23:55:13 +0100

    commit18

--- videoShow/src/package1/LancerApp1.java
+++ videoShow/src/package1/LancerApp1.java
@@ -40,8 +40,8 @@ public class LancerApp1 {
 		
 		
 		
-		//Visage v = new Visage(d);
-		//d.ajouterObjet(v);
+		Visage v = new Visage(d);
+		d.ajouterObjet(v);
 		
 		Forme f1=new Etoile(350 , 100 ,50, 8.f , Color.RED, Color.YELLOW);
 		Forme f2=new PolygoneRegulier(5 , 240 ,40, 40, 4.0f , Color.DARK_GRAY, null);
